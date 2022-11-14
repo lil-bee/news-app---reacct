d4020ed7124cb7112b3da5e2e65b82ef8d55466f
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2022-11-14 07:48:31 +0700

    error component

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Error.module.css'
+
+const Error = () => {
+    <div className={styles.container}>
+        <p className={styles.error}>Error, please reload page!</p>
+    </div>
+}
+
+export default Error
